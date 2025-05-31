d75487fef0df2328e778eb7b69d7dee5b4fd3b43
John <42081256+JohnV50@users.noreply.github.com>
2025-05-31 12:12:37 -0400

    fixed an error

--- src/modules/events/on_join.py
+++ src/modules/events/on_join.py
@@ -4,7 +4,7 @@ from resources.checks import new_counter, lookup_counter
 
 @bot.event
 async def on_guild_join(guild):
-    if await lookup_counter(guild.id) == None:
+    if await lookup_counter(guild.id) == False:
         await new_counter(guild.id)
   
     
