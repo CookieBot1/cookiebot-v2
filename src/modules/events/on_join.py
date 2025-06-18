564a489fd838498d4b3d88fc062e7f5e289f0d8d
John <42081256+JohnV50@users.noreply.github.com>
2025-06-18 11:36:05 -0400

    counter only added if cmd is run not on join

--- src/modules/events/on_join.py
+++ src/modules/events/on_join.py
@@ -1,10 +1,10 @@
 from resources.mrcookie import instance as bot
 
-from resources.checks import new_counter, lookup_counter
+from resources.checks import new_counter, new_server, lookup_counter, lookup_server
 
 @bot.event
 async def on_guild_join(guild):
-    if await lookup_counter(guild.id) == False:
-        await new_counter(guild.id)
+    if await lookup_server(guild.id) == False:
+        await new_server(guild.id)
   
     
