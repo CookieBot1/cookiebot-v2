080f3787df03f77b6a417c7e25a27e98cec31c3c
Nub <38899321+One-Nub@users.noreply.github.com>
2025-09-29 04:38:02 +0000

    Make the meme more obvious

--- src/modules/commands/drops/prompts.py
+++ src/modules/commands/drops/prompts.py
@@ -1,5 +1,5 @@
 # Format where the key is the question/prompt. The value is the accepted response.
 user_prompts: dict[str, str] = {
     "Quick! Repeat after me! Say `supercalifragilisticexpialidocious`!": "supercalifragilisticexpialidocious",
-    "What's 9+10?": "21",
+    "What's 9+10? *(hint: 'yo stupid')*": "21",
 }
