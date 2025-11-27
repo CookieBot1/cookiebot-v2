68ffbfddd7b2b8cdcc0b1e08cf6b0883b96699a7
Nub <38899321+One-Nub@users.noreply.github.com>
2025-11-27 03:35:50 +0000

    Make it REALLY annoying to try and copy and paste the answer

--- src/modules/commands/drops/prompts.py
+++ src/modules/commands/drops/prompts.py
@@ -1,5 +1,5 @@
 # Format where the key is the question/prompt. The value is the accepted response.
 user_prompts: dict[str, str] = {
-    "Quick! Repeat after me! Say `supercalifragilisticexpialidocious`!": "supercalifragilisticexpialidocious",
+    "Quick! Repeat after me! Say `​s​﻿u​﻿p​﻿e​﻿r​﻿c​﻿a​﻿l​﻿i​﻿f​﻿r​﻿a​﻿g​﻿i​﻿l​﻿i​﻿s​﻿t​﻿i​﻿c​﻿e​﻿x​﻿p​﻿i​﻿a​﻿l​﻿i​﻿d​﻿o​﻿c​﻿i​﻿o​﻿u​﻿s`!": "supercalifragilisticexpialidocious",
     "What's 9+10? *(hint: 'yo stupid')*": "21",
 }
