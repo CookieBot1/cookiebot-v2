c59023a38f797e3540629d22ca7a3c1b97920241
John <42081256+JohnV50@users.noreply.github.com>
2025-12-27 22:15:34 -0500

    command WIP

--- src/modules/commands/shop.py
+++ src/modules/commands/shop.py
@@ -1,5 +1,19 @@
 from resources.mrcookie import instance as bot
+import discord
 
 @bot.command()
 async def shop(ctx):
-    await ctx.send("test test.. this is a shop :D")
+    try:
+        await ctx.send("Welcome to the shop! Here you can spend your cookies on various items and perks. More items coming soon!")
+        give_embed = discord.Embed(
+            title = "Shop Coming Soon..",
+            color = 0x2ecc71,
+            )
+
+        give_embed.add_field(name = "", value = "test", inline = False)
+        give_embed.set_author(name = "", icon_url = ctx.author.display_avatar)
+        give_embed.set_footer(text = "test")
+        await ctx.send(embed=give_embed)
+
+    except Exception as Error:
+        await ctx.send(Error)
