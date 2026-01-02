8e6e3718ec082e7654e29660806a22dc871d52b6
John <42081256+JohnV50@users.noreply.github.com>
2026-01-01 19:24:21 -0500

    stats now shows #1 most cookies in server, also added 2 more stats to work on later

--- src/modules/commands/stats.py
+++ src/modules/commands/stats.py
@@ -1,7 +1,9 @@
 from resources.mrcookie import instance as bot
 import discord
 
-from resources.checks import lookup_counter, lookup_database, new_database
+from resources.checks import lookup_counter, lookup_server
+from typing import Optional
+from attrs import define, field
 
 @bot.command(aliases = ["serverstats", "guildstats"])
 async def stats(ctx):
@@ -14,7 +16,26 @@ async def stats(ctx):
             await new_counter(guildID)
             counterData = await lookup_counter(guildID)
 
+        ## get high score from counter data
         highScore = counterData["settings"]["counter"]["highScore"]
+
+        ## this block fetches their data from the database
+        userData = await lookup_server(guildID) 
+        if userData == False:
+            raise Exception("Insufficient server data found, try again later.")
+        
+        ## find who's #1 richest in cookies from leaderboard
+        guild_users: dict = userData.get("users", {})
+
+        if not guild_users:
+            top_uid, top_cookies = None, 0
+        else:
+            top_uid, top_data = max(
+                guild_users.items(),
+                key=lambda item: item[1].get("Cookies", 0)
+            )   
+            top_cookies = top_data.get("Cookies", 0)
+
 
         ## send the embed
         stats_embed = discord.Embed(
@@ -23,9 +44,18 @@ async def stats(ctx):
             )
 
         stats_embed.add_field(name = "🧮 Highest Number Counted", value = "Server Record: **" + str(highScore) + "**", inline = False)
-        stats_embed.add_field(name = "💰 Richest In Cookies", value = "**WIP Name** has **WIP** Cookies", inline = False)
-        stats_embed.add_field(name = "😯 Highest Robberies", value = "**WIP Name** stole **WIP** Total Cookies", inline = False)
-        stats_embed.add_field(name = "❔ WIP Title", value = "WIP Data", inline = False)
+
+        # resolve name for embed
+        if top_uid is None:
+            top_name = "Insufficient Data"
+        else:
+            user = bot.get_user(int(top_uid)) or await bot.fetch_user(int(top_uid))
+            top_name = user.global_name or user.name
+        stats_embed.add_field(name = "🍪 Richest In Cookies", value = f"**{top_name}** has **{top_cookies}** Cookie{'s' if top_cookies != 1 else ''}", inline = False)
+
+        stats_embed.add_field(name = "🎭 Most Notorious Criminal", value = "**WIP Name** robbed **WIP** times", inline = False)
+
+        stats_embed.add_field(name = "💰 Richest Thief", value = "**WIP Name** stole a total of **WIP** cookies", inline = False)
 
         stats_embed.set_thumbnail(url = guild.icon.url)
 
