11d6efaed278987a7359caa05a9689ff4021bd3b
John <42081256+JohnV50@users.noreply.github.com>
2026-01-30 16:53:19 -0500

    adjusted profile option values

--- src/resources/checks.py
+++ src/resources/checks.py
@@ -66,7 +66,7 @@ async def new_database(userID, guildID):
         "FailCounter": 0,
         "Inventory": "Empty",
         "Bio": "This user has no bio set.",
-        "ProfileOptions": {"Cookies": True, "Streaks": True, "Rank": True, "Counter Stats": True, "Count Fails": True, "Count Saves": True, "Rob Count": True, "Rob Gains": True, "Rob Chances": True},
+        "ProfileOptions": {"Cookies": True, "Streaks": True, "Counting": True, "Robbery": True, "Inventory": False},
         "ProfileColor": 0x7289da
     }
     await bot.db.update_one({"_id": str(guildID)}, {"$set": {"users." + str(userID): {**newUser}}})
