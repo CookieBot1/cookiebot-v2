eb76729508506e54ee030323982594df434351c8
John <42081256+JohnV50@users.noreply.github.com>
2026-02-05 17:03:00 -0500

    added john ID to constants

--- src/resources/constants.py
+++ src/resources/constants.py
@@ -3,6 +3,7 @@ ADMIN_USERS = (
     156872400145874944  # Nub
 )
 
+JOHN_ID = 194962036784889858 # John
 JUNO_ID = 769857438093803520  # Juno
 
 UNICODE_LEFT = "\u276e"  # left chevron, used for pagination
