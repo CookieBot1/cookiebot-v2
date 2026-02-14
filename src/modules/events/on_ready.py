a41ac244624a43a3b56af6bb05bef524b4212492
John <42081256+JohnV50@users.noreply.github.com>
2026-02-14 13:57:26 -0500

    cache refreshes everytime bot starts up to get latest dev alert

--- src/modules/events/on_ready.py
+++ src/modules/events/on_ready.py
@@ -3,6 +3,9 @@ import logging
 from discord import CustomActivity, Status
 
 from resources.mrcookie import instance as bot
+from resources.checks import lookup_bot_central
+
+from datetime import datetime, timezone
 
 
 @bot.event
@@ -12,3 +15,16 @@ async def on_ready():
         status=Status.online,
     )
     logging.info(f"{bot.user.name}#{bot.user.discriminator} is now logged in & ready!")
+
+    ## load the latest alert from the database on startup
+    data = await lookup_bot_central()
+    if data and data.get("DevAlerts"):
+        latest = data["DevAlerts"][-1]
+        exp = latest.get("expiresAt")
+
+        if exp and datetime.fromisoformat(exp) <= datetime.now(timezone.utc):
+            bot.latest_alert = None
+        else:
+            bot.latest_alert = latest
+    else:
+        bot.latest_alert = None
