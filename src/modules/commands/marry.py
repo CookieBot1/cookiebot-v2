083189bffac973ee0409e646fb017586f8ba274e
John <42081256+JohnV50@users.noreply.github.com>
2026-04-23 01:07:42 -0400

    new cmd

---
+++ src/modules/commands/marry.py
@@ -0,0 +1,63 @@
+from resources.mrcookie import instance as bot
+
+from resources.checks import lookup_database, new_database, update_value, is_blacklisted
+
+@bot.command()
+async def marry(ctx, userID = '0'):
+    try:
+        guildID = ctx.guild.id
+        guild = ctx.bot.get_guild(guildID)
+
+        userID = str(user.id)
+        senderID = str(ctx.author.id)
+
+        sender = guild.get_member(int(senderID)) or await guild.fetch_member(int(senderID))
+
+        ## validation
+        if user.id == ctx.author.id:
+            raise Exception("Invalid user, you can't give cookies to yourself!")
+
+        if guild.get_member(int(userID)) is None:
+            raise Exception("Invalid user, try again!")
+
+        if await is_blacklisted(userID):
+            raise Exception("Illegal activity! You can't give to a blacklisted user!")
+
+        ## fetching userData
+        userData = await lookup_database(userID, guildID)
+        if userData == False:
+            await new_database(userID, guildID)
+            userData = await lookup_database(userID, guildID)
+        userMarried = userData["users"][userID]["Married"]
+
+        if userMarried != 0:
+            raise Exception("This user is already married!")
+
+        ## fetching senderData
+        senderData = await lookup_database(senderID, guildID)
+        if senderData == False:
+            await new_database(senderID, guildID)
+            senderData = await lookup_database(senderID, guildID)
+        senderMarried = senderData["users"][senderID]["Married"]
+
+        if senderMarried != 0:
+            raise Exception("You are already married!")
+
+        ## marry the users
+        userMarried = senderID
+        senderMarried = userID
+
+        ## send the embed
+        marry_embed = discord.Embed(
+            title="💍 Marrying " + user.display_name + "..",
+            description=f"{sender.mention} and {user.mention} are now **MARRIED**!",
+            color=0x00ff00
+        )
+        await ctx.send(embed=marry_embed)
+
+        ## update the db
+        await update_value(userID, guildID, "Married", userMarried)
+        await update_value(senderID, guildID, "Married", senderMarried)
+
+    except Exception as Error:
+        await ctx.send(str(Error))
