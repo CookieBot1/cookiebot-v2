6e11a96e2e7c1f548d3f5dc044af8303801f9e61
John <42081256+JohnV50@users.noreply.github.com>
2026-04-23 00:38:51 -0400

    fixed bug

--- src/modules/commands/give.py
+++ src/modules/commands/give.py
@@ -1,23 +1,31 @@
 from resources.mrcookie import instance as bot
 import discord
 
-from resources.checks import lookup_database, new_database, update_value, is_blacklisted, validate_user
+from resources.checks import lookup_database, new_database, update_value, is_blacklisted
 
-@bot.command(aliases = ["transfer", "send"])
-async def give(ctx, userID = '0', amount = "0"):
+@bot.command(aliases=["transfer", "send"])
+async def give(ctx, user: discord.Member, amount: int):
     try:
         guildID = ctx.guild.id
         guild = ctx.bot.get_guild(guildID)
-        userID = await validate_user(userID)
 
-        if userID == None or userID == str(ctx.author.id) or guild.get_member(int(userID)) is None: raise Exception("Invalid user, try again!")
-        if await is_blacklisted(userID): raise Exception("Illegal activity! You can't give to a blacklisted user!")
-        if amount.isdigit() == False or int(amount) <= 0: raise Exception("Invalid amount, try again!")
-        
-        amount = int(amount)
-        user = guild.get_member(int(userID)) or await guild.fetch_member(int(userID))
+        userID = str(user.id)
         senderID = str(ctx.author.id)
+
         sender = guild.get_member(int(senderID)) or await guild.fetch_member(int(senderID))
+
+        ## validation
+        if user.id == ctx.author.id:
+            raise Exception("Invalid user, you can't give cookies to yourself!")
+
+        if guild.get_member(int(userID)) is None:
+            raise Exception("Invalid user, try again!")
+
+        if await is_blacklisted(userID):
+            raise Exception("Illegal activity! You can't give to a blacklisted user!")
+
+        if amount <= 0:
+            raise Exception("Invalid amount, try again!")
 
         ## fetching userData
         userData = await lookup_database(userID, guildID)
@@ -27,28 +35,35 @@ async def give(ctx, userID = '0', amount = "0"):
         userCookies = userData["users"][userID]["Cookies"]
 
         ## fetching senderData
-        senderData = await lookup_database(senderID, guildID) 
+        senderData = await lookup_database(senderID, guildID)
         if senderData == False:
             await new_database(senderID, guildID)
             senderData = await lookup_database(senderID, guildID)
         senderCookies = senderData["users"][senderID]["Cookies"]
 
         ## check if sender has enough cookies
-        if senderCookies < amount: raise Exception("Ahem- you don't have that many cookies..")
-        
+        if senderCookies < amount:
+            raise Exception("Ahem- you don't have that many cookies..")
+
         ## transfer the cookies
         userCookies += amount
         senderCookies -= amount
 
         ## send the embed
         give_embed = discord.Embed(
-            title = "💨 " + str(amount) + " Cookies Transferring..",
-            color = 0x2ecc71,
-            )
+            title="💨 " + str(amount) + " Cookies Transferring..",
+            color=0x2ecc71,
+        )
 
-        give_embed.add_field(name = "", value = "🍪 " + user.display_name + " now has **" + str(userCookies) + " cookies!**", inline = False)
-        give_embed.set_author(name = "", icon_url = sender.display_avatar)
-        give_embed.set_footer(text = "Cookies delivered by Sam")
+        give_embed.add_field(
+            name="",
+            value="🍪 " + user.display_name + " now has **" + str(userCookies) + " cookies!**",
+            inline=False
+        )
+
+        give_embed.set_author(name="", icon_url=sender.display_avatar)
+        give_embed.set_footer(text="Cookies delivered by Sam")
+
         await ctx.send(embed=give_embed)
 
         ## update the database
@@ -56,4 +71,4 @@ async def give(ctx, userID = '0', amount = "0"):
         await update_value(senderID, guildID, "Cookies", senderCookies)
 
     except Exception as Error:
-        await ctx.send(Error)
+        await ctx.send(str(Error))
