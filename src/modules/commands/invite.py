59569f1fa3e8f51a0fcdfe633b6a7b32ba5f28c3
John <42081256+JohnV50@users.noreply.github.com>
2026-05-03 23:29:35 -0400

    added invite link to bot

--- src/modules/commands/invite.py
+++ src/modules/commands/invite.py
@@ -4,8 +4,9 @@ from resources.mrcookie import instance as bot
 @bot.command()
 async def invite(ctx):
     invite_embed = discord.Embed(
-        description = "Invite goes here",
-        color = 0x546e7a
+        title = "Add CookieBot to your server",
+        description = "Invite CookieBot by clicking [here!](https://discord.com/oauth2/authorize?client_id=1133155318117957643)",
+        color = 0x9b59b6
         )
     invite_embed.set_author(name = ctx.bot.user.name, icon_url = ctx.bot.user.avatar)
 
